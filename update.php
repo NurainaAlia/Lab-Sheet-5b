13ada5f9c78f95020de24ef3d3db8a78acbd16cf
NurainaAlia <nabmh3007@gmail.com>
2024-12-13 01:12:39 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['matric'])) {
     $matric = $_GET['matric'];
 
